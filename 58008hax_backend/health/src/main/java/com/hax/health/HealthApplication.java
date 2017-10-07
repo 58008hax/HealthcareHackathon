6e245fb65f15702445d7d2b11d0e25b5a79c61a7
Gregory Johnson <gsoccer111@gmail.com>
2017-10-07 16:02:30 -0400

    Server start
    Includes starting code for dynamoDB connection and running the tomcat server

---
+++ 58008hax_backend/health/src/main/java/com/hax/health/HealthApplication.java
@@ -0,0 +1,13 @@
+package com.hax.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("username: user");
+		SpringApplication.run(HealthApplication.class, args);
+	}
+}
