221d9e84f3b56f3d383a8da1f89c39b1a96250eb
maquiles <mattaquiles@gmail.com>
2017-10-07 18:12:04 -0400

    dont need 2d array

--- Blockchain.java
+++ Blockchain.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Blockchain{
-    ArrayList<ArrayList<Block>> chainMaster = new ArrayList<ArrayList<Block>>();
     ArrayList<Block> chain;
 
     //to create a new chain of blocks for a new patient with a genesis block
@@ -17,20 +16,7 @@ public class Blockchain{
         this.chain.add(block);
     }
 
-    //get the index of the longest instance of the chain
-    public int getLongestChain(){
-        int longest = 0;
-        int longestIndex = -1;
-        for (int i = 0; i < chainMaster.size(); i++){
-            if(chainMaster.get(i).size() > longest){
-                longest = chainMaster.get(i).size();
-                longestIndex = i;
-            }
-        }
-        return longestIndex;
-    }
-
     public Block getBlock(int n){
-        return chainMaster.get(getLongestChain()).get(n);
+        return chain.get(n);
     }
 }
