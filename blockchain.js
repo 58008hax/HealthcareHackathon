c29df48cbf96c7eb06fbd45440a2d3b4587eb5fb
maquiles <mattaquiles@gmail.com>
2017-10-08 12:19:30 -0400

    ayy bby

--- blockchain.js
+++ blockchain.js
@@ -50,7 +50,8 @@ var initHttpServer = () => {
         res.send(sockets.map(s => s._socket.remoteAddress + ':' + s._socket.remotePort));
     });
     app.post('/addPeer', (req, res) => {
-        connectToPeers([req.body.peer]);
+        var socketStr = "ws://" + req.connection.remoteAddress + ":6001";
+        addNewPeer(socketStr);
         res.send();
     });
     app.listen(http_port, () => console.log('Listening http on port: ' + http_port));
@@ -139,11 +140,19 @@ var isValidNewBlock = (newBlock, previousBlock) => {
 
 var connectToPeers = (newPeers) => {
     newPeers.forEach((peer) => {
-        var ws = new WebSocket(peer);
+        var ws = new WebSocket(newPeers);
         ws.on('open', () => initConnection(ws));
         ws.on('error', () => {
             console.log('connection failed')
         });
+    });
+};
+
+var addNewPeer = (peer) => {
+    var ws = new WebSocket(peer);
+    ws.on('open', () => initConnection(ws));
+    ws.on('error', () => {
+        console.log('failed to add peer');
     });
 };
 
