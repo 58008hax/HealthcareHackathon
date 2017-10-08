16e04572316f77820898ee40fac10143e1c11cff
Scott Russell <scott.tkdmaster@mac.com>
2017-10-08 15:34:17 -0400

    I don't know where I am but I'm committing because i need to.

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -7,27 +7,28 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HttpService {
-    constructor(
-    private http:Http
-    ) {}
+    loginUrl:string = "http://54.242.237.38:8080";
+    blockUrl:string = "http://localhost:3001";
+
+    constructor(private http:Http) {}
 
     signUpUser(name, email, phoneNumber, street, city, state, password) {
-        return this.http.get('http://54.242.237.38:8080/user/add? name='+name+'&email='+email+'&phoneNumber='+phoneNumber+'&street='+street+'&city='+city+'&state='+state+'&password='+password).map((res:Response) => res.json());
+        return this.http.get(this.loginUrl + '/user/add?name=' + name + '&email=' + email + '&phoneNumber=' + phoneNumber + '&street=' + street + '&city=' + city + '&state=' + state + '&password=' + password).map((res:Response) => res.json());
     }
 
     checkLogin(email, password) {
-        return this.http.get('http://54.242.237.38:8080/user/check?email='+email+'&password='+password).map((res:Response) => res.json());
+        return this.http.get(this.loginUrl + '/user/check?email='+email+'&password='+password).map((res:Response) => res.json());
     }
 
     getBlocks() {
-        return this.http.get('http://54.242.237.38:3001/blocks').map((res:Response) => res.json());
+        return this.http.get(this.blockUrl + '/blocks').map((res:Response) => res.json());
     }
 
     getLastestBlock() {
-        return this.http.get('http://54.242.237.38:3001/newestBlock').map((res:Response) => res.json());
+        return this.http.get(this.blockUrl + '/newestBlock').map((res:Response) => res.json());
     }
     
     addBlock() {
-        return this.http.post('http://54.242.237.38:3001/mineBlock', '{"data":"this is a new string"}').map((res:Response) => res.json());
+        return this.http.post(this.blockUrl + '/mineBlock', '{"data":"this is a new string"}').map((res:Response) => res.json());
     }
 }
